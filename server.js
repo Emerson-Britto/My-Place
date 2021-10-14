73e65e18a27b8df9f9be3e8b8074ee6e83d1bc98
Emerson_Britto <emersonbritto987@gmail.com>
2021-10-14 05:11:28 -0300

    serve log

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 4567;
+console.log(`WEB APLICATION IS RUNNING IN PORT: ${PORT}`)
 app.listen(port);
