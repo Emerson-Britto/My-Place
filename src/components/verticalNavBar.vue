e467c3c906fd3080dce6f0b834f6f448b268c2a7
Emerson_Britto <emersonbritto987@gmail.com>
2021-10-14 05:04:14 -0300

    update navBar icons

--- src/components/verticalNavBar.vue
+++ src/components/verticalNavBar.vue
@@ -1,8 +1,8 @@
 <template>
 	<aside class="vertical_NavBar">
-		<router-link to='/'><button class="btn_navBar"><img :class="stylesNavIcon('/')" src="../assets/icons/home.svg"></button></router-link>
-		<router-link to='/projects'><button class="btn_navBar"><img :class="stylesNavIcon('/projects')" src="../assets/icons/projects.svg"></button></router-link>
-		<router-link to='/about'><button class="btn_navBar"><img :class="stylesNavIcon('/about')" src="../assets/icons/person.svg"></button></router-link>
+		<router-link to='/'><button class="btn_navBar"><img :class="stylesNavIcon('/')" src="../assets/icons/home_white_24dp.svg"></button></router-link>
+		<router-link to='/projects'><button class="btn_navBar"><img :class="stylesNavIcon('/projects')" src="../assets/icons/apps_white_24dp.svg"></button></router-link>
+		<router-link to='/about'><button class="btn_navBar"><img :class="stylesNavIcon('/about')" src="../assets/icons/account_box_white_24dp.svg"></button></router-link>
 	</aside>
 </template>
 
